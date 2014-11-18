88b38307857878198867c9febcea14d2da25ab40
Timur Borkhodoev <timur.borkhodoev@mail.utoronto.ca>
2014-11-18 13:21:04 -0500

    fixed 49 order_id not found

--- estore/application/models/order_model.php
+++ estore/application/models/order_model.php
@@ -11,7 +11,7 @@ Class Order_model extends CI_Model {
 	/*	Fetch all orders from orders table in DB 	*/
 	function getAll()
 	{
-		$query = $this->db->get('orders');			
+		$query = $this->db->get('orders');
 		return $query->result_array();
 	}
 
@@ -21,18 +21,18 @@ Class Order_model extends CI_Model {
 		$query = $this->db->get_where('order_items', array('order_id' => $id));
 		return $query->result_array('Order');
 	}
-	
+
 	function delete($id) {
 		return $this->db->delete("orders",array('id' => $id ));
 	}
-	
-	
+
+
 	function insert_order($userId, $creditCardNumber, $creditCardMonth, $creditCardYear, $total, $shoppingCart) {
 
 		/*	Generating time and date 	*/
 		$mysqlDate = date('Ymd');
 		$mysqlTime = date('H:i:s');
-		
+
 		$this->db->insert("orders", array(
 				'customer_id' => $userId,
 				'order_date' => $mysqlDate,
@@ -42,12 +42,12 @@ Class Order_model extends CI_Model {
 				'creditcard_month' => $creditCardMonth,
 				'creditcard_year' => $creditCardYear)
 		);
-		
+
 		/*	Fetching order_id of newely created order  */
 		$query = $this->db->get_where("orders",array('order_date' => $mysqlDate, 'order_time' => $mysqlTime));
 		$order_id_array = $query->result_array();
-		$order_id = $order_id[0]['id'];
-		
+		$order_id = $order_id_array[0]['id'];
+
 		/*	Inserting new items into order_items from shopping cart.  */
 		foreach ($shoppingCart as $item) {
 			$this->db->insert("order_items", array(
