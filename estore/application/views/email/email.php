8affa778296c238806f5ae5d71c74e12a1d5113d
Rakshit Shukla <shuklapossible@gmail.com>
2014-11-20 12:41:54 -0500

    changed selector class to ids and edited email markup

--- estore/application/views/email/email.php
+++ estore/application/views/email/email.php
@@ -4,11 +4,7 @@
 <meta charset="UTF-8" />
 <title>Baseball Cards Store</title>
 <!-- Latest compiled and minified CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css">
-
-<!-- Optional theme -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap-theme.min.css">
-
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
 <link rel = "stylesheet" href="<?= base_url()?>css/template.css">
 
 </head>
@@ -20,14 +16,14 @@
 	<div id="content">
 		<?php
 			foreach($allproducts as $product) {
-				echo $product['name'],"   ", $product['price'],"$<br>";
+				echo $product['name'],"   $", $product['price'],"<br>";
 			}
 		?>
 	</div>
 	<hr>
 	<div id=total>
 		<?php
-			echo "Your total: ", $orders[0]['total'];
+			echo "Your total is: $", $orders[0]['total'];
 		?>
 	</div>
 </body>
